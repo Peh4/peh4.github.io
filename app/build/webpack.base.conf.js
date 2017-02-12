b8d2fa1e070c9b3cb24aec834903c1d86cf560f2
Peh4 <peha@free.fr>
2017-02-12 11:06:32 +0100

    fix font-awesome

--- app/build/webpack.base.conf.js
+++ app/build/webpack.base.conf.js
@@ -36,9 +36,16 @@ module.exports = {
     loaders: [
       {
         test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
-        test: /\.json/,
-        loader: 'json',
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      { test: /\.json/,
+        loader: 'json'
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "url-loader?limit=10000&minetype=application/font-woff" 
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "file-loader" 
       }
     ],
     rules: [
